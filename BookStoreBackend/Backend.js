2672d51878f78e51be2564c6b554610acc94eba1
jacob460 <65002885+jacob460@users.noreply.github.com>
2024-12-03 03:01:02 -0500

    Bunch of changes
    everything
    can create customer in db
    login as user

--- BookStoreBackend/Backend.js
+++ BookStoreBackend/Backend.js
@@ -1,25 +1,53 @@
-var mysql = require('mysql');
-var http = require('http');
+import mysql from 'mysql2';
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Fuck you');
-    res.write(req.url);
-    res.end();
-}).listen(8080);
+var pool
+    /*var pool = mysql.createPool({
+        host: "localhost",
+        user: "s",
+        password: "password",
+        database: "bookstore",
+    }).promise();
+        console.log(error)
+    */
+export async function tryConnection(username, password){
+    pool = mysql.createPool({
+        host: "localhost",
+        user: username,
+        password: password,
+        database: "bookstore", 
+    }).promise();
+    
+    const test = pool.query("SHOW GRANTS FOR " + username + "@localhost");
+    //const test = queryBookstore("SELECT * FROM bookdata LIMIT 15")
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "Jacob",
-    password: "password",
-    database: "bookstore",
-});
-var sql = "SELECT * FROM bookdata";
-con.connect(function(err) {
-    if(err) throw err;
-    console.log("Connected");
-    con.query(sql, function (err, result){
-        if(err) throw err;
-        console.log("Result: " + result)
-    });
-});
+    return(test)
+}
+export async function createUser(username, fname, lname, password, addr, phoneNum){
+    pool = mysql.createPool({
+        host: "127.0.0.1",
+        user: "root",
+        password: "Mechromancer1427",
+        database: "bookstore", 
+    }).promise();
+    console.log("CREATE USER:")
+    console.log(username);
+    console.log(password);
+    console.log(addr);
+    console.log(fname);
+    console.log(lname);
+    console.log(phoneNum);
+    const test = pool.query(`insert into customers (username, fname, lname, password, address, phoneNumber)values (\"${username}\", \"${fname}\", \"${lname}\", \"${password}\", \"${addr}\", \"${phoneNum}\")`);
+
+    return(test)
+
+}
+
+
+export async function queryBookstore(query){
+    const result = await pool.query(query);
+    //console.log(result[0]);
+    return(result);
+}
+
+//const notes = await queryBookstore("SELECT * FROM bookdata LIMIT 10");
+//console.log(notes);
---
+++ BookStoreBackend/app.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import { createUser, queryBookstore, tryConnection } from './Backend.js';
+import cors from 'cors'
+
+const app = express();
+
+app.use(cors())
+
+app.get('/', async (req, res)=>{
+    console.log(req.query.username)
+    console.log(req.query.password)
+    //const result = await queryBookstore("SELECT * FROM bookdata LIMIT 10");
+    //console.log(result[0])
+    const result = await tryConnection(req.query.username, req.query.password);
+    //console.log(result)
+    
+    res.send(result)
+})
+
+app.get('/signUp', async (req, res)=>{
+    console.log(req.query)
+    console.log(req.query.username) 
+    console.log(req.query.fname) 
+    console.log(req.query.lname)
+    console.log(req.query.password) 
+    console.log(req.query.addr) 
+    console.log(req.query.phoneNum)
+    //const result = await queryBookstore("SELECT * FROM bookdata LIMIT 10");
+    //console.log(result[0])
+    const result = await createUser(req.query.username, req.query.fname, req.query.lname, req.query.password, req.query.addr, req.query.phoneNum);
+    //console.log(result)
+    
+    res.send(result)
+})
+
+app.use((err, req, next) => {
+    //console.error(err.stack);
+    //resizeBy.status(500).send('something broke!')
+})
+
+app.listen(8080, ()=>{
+    console.log('Server is running on port 8080')
+})
